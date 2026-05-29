9a95ae53e72b62120103d0563508c19726dc5cd8
Aswitha s <aswithaksraja@gmail.com>
2026-05-29 19:14:53 +0530

    fixed render port

--- app.py
+++ app.py
@@ -63,6 +63,7 @@ def predict():
     except Exception as e:
         return f"Error: {e}"
 
-# Run app
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
